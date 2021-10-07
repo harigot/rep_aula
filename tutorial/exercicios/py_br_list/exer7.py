5550ea9418c442f0bd50920ad1738bc1097f851a
jcamachadopc <jose.machado@precocerto.co>
2021-10-07 20:24:56 -0300

    feat: ex7 review

--- tutorial/exercicios/py_br_list/exer7.py
+++ tutorial/exercicios/py_br_list/exer7.py
@@ -1,12 +1,12 @@
-#Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
+# Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
 
 list = [1, 1, 1, 2, 2]
 
 sum = sum(list)
-print(sum)
+print(sum)  # Correção: Falta de legibilidade -0,5
 multiply = 1
 
 for i in range(len(list)):
     multiply *= list[i]
 
-print(multiply)
+print(multiply)  # Correção: Falta de legibilidade -0,5
