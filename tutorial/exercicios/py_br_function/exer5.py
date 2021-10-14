64151831191717da3fde408521bf58c6c59384ed
harigot <ofbrunobm@gmail.com>
2021-10-14 19:50:39 -0300

    feat: hg functions homework exer5

---
+++ tutorial/exercicios/py_br_function/exer5.py
@@ -0,0 +1,13 @@
+#Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais:
+#taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem e custo, que é 
+#o custo de um item antes do imposto. A função “altera” o valor de custo para incluir o 
+#imposto sobre vendas.
+
+cost = float(input('Preco do produto: '))
+tax = float(input('Taxa de imposto: '))
+
+
+def somaimposto(tax, cost):
+    return (1 + tax/100)*cost
+
+print('Valor com imposto:', somaimposto(tax,cost))
