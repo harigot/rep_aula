52c166ddc833d97dce4c926c0ce5e5f8ae5bfc10
machadojose <jcdealmeidam@gmail.com>
2021-10-14 21:32:43 -0300

    refactor: remove alias for Exception

--- tutorial/examples/try_except_demo.py
+++ tutorial/examples/try_except_demo.py
@@ -1,6 +1,3 @@
-import time
-
-
 def foo(str_num):
     if str_num is int:
         raise ValueError("Número não é string")
@@ -12,12 +9,9 @@ try:
     foo("dez")
 except ZeroDivisionError as e:
     print("error division by zero")
-except Exception as e:
-    print(e.__class__)
+except Exception:
+    print(Exception.__class__)
 else:
     print("Deu certo, valeu")
 finally:
     print("Tchau")
-
-time.sleep(10)
-print("Agora acabou")
